5e197476c1947f0a4285961d39a14b2bf3459e84
josiplisec <josiplisec@c90cfa4e-172e-0410-9d97-c17d952d65b0>
2007-08-14 10:14:57 +0000

    Copyright updates

--- admin/footer.php
+++ admin/footer.php
@@ -1,7 +1,7 @@
 		<div id="footer">
 			Powered by
 			<a href="http://planetoid-project.org/" title="Planetoid">Planetoid</a>
-			<?=PLANETOID_VERSION?>.<?=PLANETOID_REVISION?>, &copy; 2007 Josip Lisec
+			<?=PLANETOID_VERSION?>.<?=PLANETOID_REVISION?>, &copy; 2007 Planetoid project
 		</div>
 	</body>
 </html>
